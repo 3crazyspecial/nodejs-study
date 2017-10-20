13cffc973ac6b30d1df8b3448c02ce75fbac459d
jie.zhang <jie.zhang@hobot.cc>
2017-10-20 19:24:14 +0800

    article

--- model/post.js
+++ model/post.js
@@ -57,13 +57,13 @@ Post.prototype.save = function(callback) {
 }
 
 // 读取文章和其他相关信息
-Post.get = function(name, callback) {
+Post.getAll = function(name, callback) {
 	// 打开数据库
 	mongodb.open(function(err, db) {
 		if(err) {
 			return callback(err);
 		}
-			// 读取post集合
+		// 读取post集合
 		db.collection('posts', function(err, collection) {
 			if(err) {
 				mongodb.close();
@@ -90,6 +90,35 @@ Post.get = function(name, callback) {
 			})
 		})
 	})
-
-
 }
+
+Post.getOne = function(name, day, title, callback) {
+	// 打开数据库
+	mongodb.open(function(err, db) {
+		if(err) {
+			return callback(err);
+		}
+		// 读取post集合
+		db.collection('posts', function(err, collection) {
+			if(err) {
+				mongodb.close();
+				return callback(err);
+			}
+			// 根据用户名，发表日期，和文章名进行查询
+			collection.findOne({
+				"name": name,
+				"time.day": day,
+				"title": title
+			}, function(err, doc) {
+				mongodb.close();
+				if(err) {
+					return callback(err);
+				}
+
+				// 解析markdown 为html
+				doc.post = markdown.toHTML(doc.post);
+				callback(null, doc); // 返回查询到的一篇文章
+			})
+		})
+	})
+} 
