9cf8ff4b0bb07c61e7fd032f85c7b96f0bb66f36
jie.zhang <jie.zhang@hobot.cc>
2017-10-26 00:20:41 +0800

    headimg

--- routes/index.js
+++ routes/index.js
@@ -137,7 +137,7 @@ module.exports = function(app) {
 	app.post('/post', function(req, res) {
 		var currentUser = req.session.user,
 			tags = [req.body.tag1, req.body.tag2, req.body.tag3],
-			post = new Post(currentUser.name, req.body.title, tags, req.body.post);
+			post = new Post(currentUser.name, currentUser.head, req.body.title, tags, req.body.post);
 		post.save(function(err) {
 			if(err) {
 				req.flash('error', err);
@@ -322,6 +322,31 @@ module.exports = function(app) {
 		})
 	})
 
+	app.post('/u/:name/:day/:title', function(req, res) {
+		var date = new Date(),
+			time = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+		var md5 = crypto.createHash('md5'),
+			email_MD5 = md5.update(req.body.email.toLowerCase()).digest('hex'),
+			head = 'www.gravatar.com/avatar/' + email_MD5 + '?s=48';
+		var comment = {
+			name: req.body.name,
+			head: head,
+			email: req.body.email,
+			website: req.body.website,
+			time: time,
+			content: req.body.content
+		};
+		var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+		newComment.save(function(err) {
+			if(err) {
+				req.flash('error', err);
+				res.redirect('back');
+			}
+			req.flash('success', '留言成功');
+			res.redirect('back');
+		})
+	})
+
 	app.get('/edit/:name/:day/:title', checkLogin);
 	app.get('/edit/:name/:day/:title', function(req, res) {
 		console.log('aaa')
@@ -364,27 +389,6 @@ module.exports = function(app) {
 			}
 			req.flash('success', '删除成功');
 			res.redirect('/');
-		})
-	})
-
-	app.post('/u/:name/:day/:title', function(req, res) {
-		var date = new Date(),
-			time = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
-		var comment = {
-			name: req.body.name,
-			email: req.body.email,
-			website: req.body.website,
-			time: time,
-			content: req.body.content
-		};
-		var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
-		newComment.save(function(err) {
-			if(err) {
-				req.flash('error', err);
-				res.redirect('back');
-			}
-			req.flash('success', '留言成功');
-			res.redirect('back');
 		})
 	})
 
